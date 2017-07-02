06606d39c34fe361664cbc7b11e1924410c84de4
Iku Iwasa <iku.iwasa@gmail.com>
2017-07-02 15:18:57 +0900

    Specify input SQL by command line
    instead of in Dockerfile.

--- convertdb.py
+++ convertdb.py
@@ -6,23 +6,19 @@ from lib.mysql import MySQL
 from lib.postgres import Postgres
 from lib.redmine import Redmine
 
-in_sqlfile = Path('mysql/redmine.sql')
-if not in_sqlfile.exists():
-    print('Put SQL file: {0}'.format(str(in_sqlfile)))
+if len(sys.argv) < 3:
+    print('Usage: {0} INPUT_SQL_FILE OUTPUT_SQL_FILE'.format(sys.argv[0]))
     exit(1)
 
-if len(sys.argv) < 2:
-    print('Specify output SQL file')
-    exit(1)
-
-out_sqlfile = sys.argv[1]
+in_sqlfile  = sys.argv[1]
+out_sqlfile = sys.argv[2]
 
 client = docker.from_env()
 
 volume = client.volumes.create('redmine-yaml_db-data')
 
 mysql = MySQL(client)
-mysql.load_db()
+mysql.load_db(in_sqlfile)
 print('Created MySQL container: {0}'.format(mysql.container.short_id))
 
 redmine = Redmine(client)
--- lib/mysql.py
+++ lib/mysql.py
@@ -1,3 +1,5 @@
+from . import utils
+
 class MySQL:
     __name = 'redmine-yaml_db-mysql'
     __envs = [ 'MYSQL_ALLOW_EMPTY_PASSWORD=yes',
@@ -12,19 +14,22 @@ class MySQL:
     def __build(self):
         self.client.images.build(path = 'mysql', rm = True, tag = MySQL.__name)
 
-    def load_db(self):
+    def load_db(self, sqlfile):
         self.container = self.client.containers.run(MySQL.__name,
                                                     detach = True,
                                                     environment = MySQL.__envs)
 
         ready = 0
-        for line in self.container.logs(stdout = False,
-                                        stderr = True,
-                                        stream = True):
-            if b'redmine.sql' in line or b'ready for connections' in line:
+        for line in self.container.logs(stdout = False, stderr = True, stream = True):
+            if b'ready for connections' in line:
                 ready += 1
             if ready >= 2:
                 break
+
+        utils.docker_copyto(self.container, sqlfile, '/tmp/')
+        out = self.container.exec_run('mysql -u root redmine -e "source /tmp/redmine.sql"', tty = True)
+        if len(out) > 0:
+            print(out)
 
     def release(self):
         self.container.stop()

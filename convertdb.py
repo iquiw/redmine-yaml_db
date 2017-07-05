533bb21bd36225823125702372f5238292e90e05
Iku Iwasa <iku.iwasa@gmail.com>
2017-07-05 23:52:41 +0900

    Support Redmine plugins container via dotenv

--- convertdb.py
+++ convertdb.py
@@ -1,5 +1,7 @@
 import docker
+import os
 import sys
+from dotenv import load_dotenv, find_dotenv
 
 from lib.mysql import MySQL
 from lib.postgres import Postgres
@@ -12,16 +14,21 @@ if len(sys.argv) < 3:
 in_sqlfile  = sys.argv[1]
 out_sqlfile = sys.argv[2]
 
+load_dotenv(find_dotenv())
+PLUGIN_IMAGE_NAME = os.environ.get('REDMINE_PLUGINS_IMAGE')
 client = docker.from_env()
 
 volume = client.volumes.create('redmine-yaml_db-data')
+plugins = None
+if PLUGIN_IMAGE_NAME is not None:
+    plugins = client.containers.run(PLUGIN_IMAGE_NAME, detach = True)
 
 mysql = MySQL(client)
 mysql.load_db(in_sqlfile)
 print('Created MySQL container: {0}'.format(mysql.container.short_id))
 
 redmine = Redmine(client)
-redmine.dump_yaml(mysql, volume)
+redmine.dump_yaml(mysql, volume, plugins)
 print('MySQL DB dumped to volume: {0}'.format(volume.name))
 
 mysql.release()
@@ -30,7 +37,7 @@ postgres = Postgres(client)
 postgres.run()
 print('Created PostgreSQL container: {0}'.format(postgres.container.short_id))
 
-redmine.load_yaml(postgres, volume)
+redmine.load_yaml(postgres, volume, plugins)
 print('PostgreSQL DB loaded from volume: {0}'.format(volume.name))
 
 postgres.dump_db(out_sqlfile)
--- lib/redmine.py
+++ lib/redmine.py
@@ -8,7 +8,7 @@ class Redmine:
     def __build(self):
         self.client.images.build(path = 'redmine', rm = True, tag = Redmine.__name)
 
-    def __run(self, command, environment, dbname, volume):
+    def __run(self, command, environment, dbname, volume, plugins):
         self.client.containers.run(
             Redmine.__name,
             command = command,
@@ -16,20 +16,23 @@ class Redmine:
             entrypoint = '/bin/sh',
             links = [(dbname, 'db')],
             remove = True,
-            volumes = { volume.name: { 'bind': '/data', 'mode': 'rw' }})
+            volumes = { volume.name: { 'bind': '/data', 'mode': 'rw' }},
+            volumes_from = [ plugins.name ] if plugins is not None else None)
 
-    def dump_yaml(self, mysql, volume):
+    def dump_yaml(self, mysql, volume, plugins):
         self.__run('/dump.sh',
                    [ 'REDMINE_DB_MYSQL=' + mysql.container.name,
                      'REDMINE_DB_USERNAME=redmine',
                      'REDMINE_DB_PASSWORD=redmine' ],
                    mysql.container.name,
-                   volume)
+                   volume,
+                   plugins)
 
-    def load_yaml(self, postgres, volume):
+    def load_yaml(self, postgres, volume, plugins):
         self.__run('/load.sh',
                    [ 'REDMINE_DB_POSTGRES=' + postgres.container.name,
                      'REDMINE_DB_USERNAME=redmine',
                      'REDMINE_DB_PASSWORD=redmine' ],
                    postgres.container.name,
-                   volume)
+                   volume,
+                   plugins)
